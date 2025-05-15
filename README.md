426319712a511335d383482957c57a5a43f206b4
hyeonnnho <tiger1330@naver.com>
2025-05-15 16:21:49 +0900

    6일차 커밋

--- README.md
+++ README.md
@@ -351,4 +351,20 @@ https://github.com/user-attachments/assets/4b6bb5af-1be7-4b67-a6a2-7d3f6d4a836a
 ### MovieFinder 2025 (계속)
 1. 상태표시줄 시계 동작
 2. 상태표시줄 검색 결과 건수 표시
-3. 즐겨찾기 DB연동...
+3. 로그 출력 정리
+4. 즐겨찾기 DB연동...
+    1. MySQL Workbench에서 moviefinder 데이터베이스(스키마) 생성
+    2. movieitems 테이블 생성. 컬럼은 MovieItem.cs 속성과 동일
+    3. INSERT, UPDATE, DELETE 작업 완료
+
+    <img src="./image/wpf0017.png" width="650">
+5. Youtube 예고편 보기
+    1. TrailerView, TrailerViewModel
+    2. WPF 기본 WebBrowser는 HTML5 기술이 표현 안됨. 오류가 많음
+    3. NuGet 패키지 - CefSharp WebBrowser 패키지 설치
+    4. CefSharp.Wpf 설치 시 프로젝트 속성 > 빌드 > 일반, 플랫폼 대상을 Any CPU에서 x64로 변경 
+    5. NuGet 패키지 -> Google.Apis. youtube c3설치
+
+6. 기타 작업 완료
+7. 결과 화면
+ 
---
+++ day06/Day06Study/MovieFinder2025/ViewModels/TrailerViewModel.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.ObjectModel;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using MahApps.Metro.Controls.Dialogs;
+using Microsoft.VisualBasic;
+using MovieFinder2025.Helpers;
+using MovieFinder2025.Models;
+
+namespace MovieFinder2025.ViewModels
+{
+    public partial class TrailerViewModel : ObservableObject
+    {
+        private readonly IDialogCoordinator dialogCoordinator;
+
+        public TrailerViewModel(IDialogCoordinator coordinator, string movieTitle)
+        {
+            this.dialogCoordinator = coordinator;
+            MovieTitle = movieTitle;
+
+            YoutubeUri = "https://www.youtube.com";
+            SearchYoutubeApi();
+        }
+
+        private string _movieTitle;
+
+        public string MovieTitle
+        {
+            get => _movieTitle;
+            set => SetProperty(ref _movieTitle, value);
+        }
+
+        private ObservableCollection<YoutubeItem> _youtubeItems;
+
+        public ObservableCollection<YoutubeItem> YoutubeItems
+        {
+            get => _youtubeItems;
+            set => SetProperty(ref _youtubeItems, value);
+        }
+
+        private YoutubeItem _selectedYoutube;
+        public YoutubeItem SelectedYoutube
+        {
+            get => _selectedYoutube;
+            set => SetProperty(ref _selectedYoutube, value);
+        }
+
+        private string _youtubeUri;
+        public string YoutubeUri
+        {
+            get => _youtubeUri;
+            set => SetProperty(ref _youtubeUri, value);
+        }
+
+        /// <summary>
+        /// 핵심 Youtube Data Api v3 호출
+        /// </summary>
+        private async Task SearchYoutubeApi()
+        {
+            await LoadDataCollection();
+        }
+
+        private async Task LoadDataCollection()
+        {
+            var service = new YouTubeService(
+                new BaseClientService.Initializer()
+                {
+                    ApiKey = "AIzaSyAVIUh2S1ymzKnr8-ETXRkaoE3VxFb1Oc4",
+                    ApplicationName = this.GetType().ToString()
+                });
+            var req = service.Search.List("snippet");
+            req.Q = $"{MovieTitle} 예고편 공식";   // 영화이름만 사용해서 API를 검색
+            req.Order = SearchResource.ListRequest.OrderEnum.Relevance;
+            req.Type = "video";
+            req.MaxResults = 10;
+
+            ObservableCollection<YoutubeItem> youtubeItems = new ObservableCollection<YoutubeItem>();
+
+            var res = await req.ExecuteAsync(); // Youtube API 서버에 요청된 값 실행하고 결과를 리턴(비동기)
+            foreach (var item in res.Items)
+            {
+                youtubeItems.Add(new YoutubeItem
+                {
+                    Title = item.Snippet.Title,
+                    ChannelTitle = item.Snippet.ChannelTitle,
+                    URL = $"https://www.youtube.com/watch?v={item.Id.VideoId}", // Youtube Play Link
+                    Author = item.Snippet.ChannelId,
+
+                    Thumbnail = new BitmapImage(new Uri(item.Snippet.Thumbnails.Default__.Url, UriKind.RelativeOrAbsolute))
+                });
+            }
+
+            YoutubeItems = youtubeItems;
+            Common.LOGGER.Info($"{MovieTitle}의 예고편 {YoutubeItems.Count} 조회 완료");
+
+        }
+
+        [RelayCommand]
+        public async Task YoutubeDoubleClick()
+        {
+            YoutubeUri = SelectedYoutube.URL;
+        }
+    }
+}
